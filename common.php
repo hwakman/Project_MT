46b35d1122ddc54f44188d04ca55f6a297853ea6
hwakman <pa.chayoud_st@tni.ac.th>
2017-04-26 15:51:17 +0700

    add file

---
+++ common.php
@@ -0,0 +1,10 @@
+<html>
+<link href="Template/css/base.css" rel="stylesheet" type="text/css">
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: internship
+ * Date: 26/4/2560
+ * Time: 15:38 น.
+ */
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<html>
-    <link href="Template/css/base.css">
-</html>
 <?php
+require_once "common.php";
 require_once "config/config.php";
 require_once "Template/main.php";
 ?>
