3690cee42177b38d5a859ecab7f3dd0a2af92bf5
hwakman <pa.chayoud_st@tni.ac.th>
2017-05-18 10:25:15 +0700

    add

--- index.php
+++ index.php
@@ -3,7 +3,12 @@ session_start();
 ?>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <title>Login User</title>
     <link href="Template/css/base.css" rel="stylesheet" type="text/css">
+    <style>
+        .AutoSlide {display: none;}
+    </style>
 </head>
 <body>
     <table style="float: right">
@@ -27,6 +32,29 @@ session_start();
     <div style="clear: right"></div>
 <table width="100%" style="height: 500px" >
     <tr>
+        <th>
+            <div style="margin: auto">
+                <img src="img/02.jpg" style="width: 100%" class="AutoSlide">
+                <img src="img/04.jpg" style="width: 100%" class="AutoSlide">
+                <img src="img/05.jpg" style="width: 100%" class="AutoSlide">
+                <img src="img/09.jpg" style="width: 100%" class="AutoSlide">
+            </div>
+        </th>
+        <script>
+            var myIndex = 0;
+            slideShow();
+            function slideShow() {
+                var i;
+                var x = document.getElementsByClassName("AutoSlide");
+                for(i=0;i<x.length;i++){
+                    x[i].style.display = "none";
+                }
+                myIndex++;
+                if (myIndex > x.length){myIndex = 1}
+                x[myIndex-1].style.display = "block";
+                setTimeout(slideShow,5000);
+            }
+        </script>
     </tr>
 </table>
 </body>
